7895557f3b2fea3f468d4ee105e8e47ea040ba6a
vaibhav-sharma-oss <80534369+iamvaibhav31@users.noreply.github.com>
2022-03-30 03:00:27 +0530

    Starting Chat Applications

--- server/Server.js
+++ server/Server.js
@@ -5,9 +5,29 @@ const http = require("http")
 
 const cors = require("cors")
 
+const { Server } = require("socket.io")
+
 app.use(cors())
 
-const server = http.createServer(app)
-server.listen(3001,()=>{
+const httpserver = http.createServer(app)
+
+const io = new Server(httpserver,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(socket.id)
+
+
+    socket.on("disconnect",()=>{
+        console.log("user disconect: ",socket.id)
+    })
+})
+
+httpserver.listen(3001,()=>{
+
     console.log("server runing")
 })
